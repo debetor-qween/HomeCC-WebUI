14069a2aa8c64cdfbcef9f95ffa9e3f6ad1b6345
Сергей Новиков <debetor@mail.ru>
2021-05-26 12:59:38 +0300

    Удалил Helloapp.vue (рудимент из шаблона)

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
   <div id="app">
-    <!--
-    <img alt="Vue logo" src="./assets/logo.png">
-    -->
     <NavMenu msg="Строка меню"/>
     <SettingsPage msg="Строка меню"/>
     <!--HelloWorld msg="Welcome to Your Vue.js App"/-->
@@ -12,14 +9,11 @@
 <script>
 import NavMenu from './components/NavMenu.vue'
 import SettingsPage from './components/Settings.vue'
-// import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
     NavMenu,
     SettingsPage,
-    //,     HelloWorld
   }
 }
 </script>
